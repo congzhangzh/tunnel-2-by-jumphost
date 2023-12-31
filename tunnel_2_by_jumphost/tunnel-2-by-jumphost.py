f8a2bc4d16f9baf294c6f74086618f782074c33c
Cong Zhang <13283869+congzhangzh@users.noreply.github.com>
2023-12-31 06:36:09 -0800

    [eng] improve log

--- tunnel_2_by_jumphost/tunnel-2-by-jumphost.py
+++ tunnel_2_by_jumphost/tunnel-2-by-jumphost.py
@@ -1,8 +1,28 @@
 import traceback
+import logging
 import fire
 import asyncio
 from functools import partial
 from typing import Union
+
+class AsyncioTaskFilter(logging.Filter):
+    def filter(self, record):
+        task = asyncio.current_task()
+        record.task_info = f'Task id={id(task)}' if task else 'No current task'
+        return True
+
+# 创建日志记录器，设置日志级别为DEBUG
+logger = logging.getLogger('my_logger')
+logger.setLevel(logging.DEBUG)
+logger.addFilter(AsyncioTaskFilter())
+
+# 创建一个日志处理器，并设置格式化字符串
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s [%(task_info)s] (%(filename)s:%(lineno)d)')
+handler.setFormatter(formatter)
+
+# 将处理器添加到日志记录器
+logger.addHandler(handler)
 
 """
 Design Notes:
@@ -19,15 +39,15 @@ async def forward_data(reader1, writer1, reader2, writer2):
 
 async def proxy_data(reader, writer):
     while True:
-        # print(f"--begin-- read, {reader.transport.get_extra_info('peername')}")
-        print(f"--begin-- read")
+        # logger.debug(f"--begin-- read, {reader.transport.get_extra_info('peername')}")
+        logger.debug(f"--begin-- read")
         data = await reader.read(4096)
-        print(f'--end-- read {data}')
+        logger.debug(f'--end-- read {data}')
         if not data:
             break
-        print(f"--begin-- write, {writer.transport.get_extra_info('peername')}")
+        logger.debug(f"--begin-- write, {writer.transport.get_extra_info('peername')}")
         writer.write(data)
-        print(f'--begin-- write')
+        logger.debug(f'--begin-- write')
 
         await writer.drain()
 
@@ -35,7 +55,7 @@ async def handle_client_connection(tunnel_connection_queue, reader, writer):
     transport = writer.transport
     client_ip, client_port = transport.get_extra_info('peername')
     server_ip, server_port = transport.get_extra_info('sockname')
-    print(f"[server][tunn conn] receive connected to tunnel port {server_ip}:{server_port}, from {client_ip}:{client_port}")
+    logger.debug(f"[server][tunn conn] receive connected to tunnel port {server_ip}:{server_port}, from {client_ip}:{client_port}")
     
     await tunnel_connection_queue.put((reader, writer))
 
@@ -43,18 +63,18 @@ async def handle_external_connection(tunnel_connection_queue, reader, writer):
     transport = writer.transport
     client_ip, client_port = transport.get_extra_info('peername')
     server_ip, server_port = transport.get_extra_info('sockname')
-    print(f"[server][external conn]receive connected to external port {server_ip}:{server_port}, from {client_ip}:{client_port}")
+    logger.debug(f"[server][external conn]receive connected to external port {server_ip}:{server_port}, from {client_ip}:{client_port}")
 
-    print(f'[server][relay] --begin-- try to get one tunnel from queue')
+    logger.debug(f'[server][relay] --begin-- try to get one tunnel from queue')
     tunnel_reader, tunnel_writer = await tunnel_connection_queue.get()
-    print(f'[server][relay] --end-- try to get one tunnel from queue')
+    logger.debug(f'[server][relay] --end-- try to get one tunnel from queue')
     #!!!Trick, tell client this tunnel will be used, prepare another tunnel
-    print(f'[server][relay] --begin-- try to send {READY_MARK}')
+    logger.debug(f'[server][relay] --begin-- try to send {READY_MARK}')
     await tunnel_writer.write(READY_MARK)
     await writer.drain()
-    print(f'[server][relay] --end-- try to send {READY_MARK}')
+    logger.debug(f'[server][relay] --end-- try to send {READY_MARK}')
     
-    print(f'two way community between external connection and tunnel connection')
+    logger.debug(f'two way community between external connection and tunnel connection')
     # with  tunnel_writer, writer:
     await forward_data(reader, tunnel_writer, tunnel_reader, writer)
     
@@ -97,13 +117,13 @@ async def _c(jump_host, jump_host_tunnel_port, relay_to_host, relay_to_port):
             reader, writer = await asyncio.open_connection(jump_host, jump_host_tunnel_port)
             ready_mark=await reader.readexactly(len(READY_MARK))
             if ready_mark==READY_MARK:
-                print("[client][relay mark]reday mark receive! forward data now!")
+                logger.debug("[client][relay mark]reday mark receive! forward data now!")
             else:
                 raise Exception("[client][relay mark]unexpected data received!")
             tunnel_reader, tunnel_writer = await asyncio.open_connection(relay_to_host, relay_to_port)
             
             #with tunnel_writer, writer:
-            print(f'two way community between tunnel connection and redirect connection')
+            logger.debug(f'two way community between tunnel connection and redirect connection')
             await forward_data(reader, tunnel_writer, tunnel_reader, writer)
             
             # #TODO deadlock possible?
@@ -114,10 +134,10 @@ async def _c(jump_host, jump_host_tunnel_port, relay_to_host, relay_to_port):
 
             time_to_wait=0
         except Exception as e:
-            print(f"[client] Error connecting to the server: {traceback.format_exc()}")
+            logger.debug(f"[client] Error connecting to the server: {traceback.format_exc()}")
             time_to_wait=time_to_wait+1
             await asyncio.sleep(time_to_wait)
-            print(f"[client] wakeup from sleep {time_to_wait}")
+            logger.debug(f"[client] wakeup from sleep {time_to_wait}")
 
 if __name__ == "__main__":
     fire.Fire({
